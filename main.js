e6d7b8965d537f71da5c60ec9451107bfb6c59ea
Harry Keartland <harrykeartland@gmail.com>
2018-06-06 17:43:29 +0100

    added function chooser

--- main.js
+++ main.js
@@ -1,23 +1,34 @@
 var elt = document.getElementById('calculator');
 var calculator = Desmos.Calculator(elt);
-calculator.setExpression({id:'graph1', latex:'cos(sin(x))'});
-
-
-function f(x){
-  return Math.cos(Math.sin(x))
-}
 
 function calcabc(h0,h1,h2,i){
   c = h1
   a = (h0 - (2*h1) + h2)/(2*h*h)
   b = (h0-h1-(a*h*h))/ -h
-  eq = "0 < y < " + a + "(x - " + (i + 1)*h + ")^2 + "+ b + "(x - " + (i + 1)*h  +") + "+ c + "\\left\\{"+ i*h + " < x < "+(i + 2)*h+"\\right\\}"
+  eq = "y <= " + a + "(x - " + (i + 1)*h + ")^2 + "+ b + "(x - " + (i + 1)*h  +") + "+ c + "\\left\\{"+ i*h + " <= x <= "+(i + 2)*h+"\\right\\}"
   calculator.setExpression({id:i, latex:eq});
 }
-h = 0.4
-for (i = 0; i < 2*16;i+=2){
-  h0 = f((i)*h)
-  h1 = f((i+1)*h)
-  h2 = f((i+2)*h)
-  calcabc(h0,h1,h2,i)
+function run(){
+  h = 0.4
+  amount = 64
+  for (i = 0; i < 2*amount;i+=2){calculator.setExpression({id:i, latex:""});}
+  for (i = 0; i < 2*amount;i+=2){
+    if (document.getElementById("selFunction").value == "cos(x)"){
+      calculator.setExpression({id:"f", latex:document.getElementById("selFunction").value});
+      h0 = Math.cos((i)*h)
+      h1 = Math.cos((i+1)*h)
+      h2 = Math.cos((i+2)*h)
+    }if (document.getElementById("selFunction").value == "sin(x)"){
+      calculator.setExpression({id:"f", latex:document.getElementById("selFunction").value});
+      h0 = Math.sin((i)*h)
+      h1 = Math.sin((i+1)*h)
+      h2 = Math.sin((i+2)*h)
+    }if (document.getElementById("selFunction").value == "tan(x)"){
+      calculator.setExpression({id:"f", latex:document.getElementById("selFunction").value});
+      h0 = Math.tan((i)*h)
+      h1 = Math.tan((i+1)*h)
+      h2 = Math.tan((i+2)*h)
+    }
+    calcabc(h0,h1,h2,i)
+  }
 }
